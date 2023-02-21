f916a0938a9158f5b9ae3e2f487f5ad5b268ecd6
Terre8055 <michaelappiah2018@icloud.com>
2023-02-21 19:32:53 +0000

    Update code with comments

--- script.js
+++ script.js
@@ -1,47 +1,51 @@
 window.onload = () => {
 
-function setTime(){
-    const date = new Date();
+  function setTime(){
+    // call Date Api
+    const date = new Date(); 
     var hours = date.getHours();
     var minutes = date.getMinutes();
     var seconds = date.getSeconds()
-    let time = `${hours} : ${minutes} : ${seconds}`
+
+    let time = `${hours} : ${minutes} : ${seconds}` //eg: 12:12:06
     
+    //DOM Manipulation
     let showTime = document.getElementById('container');
-    
-    showTime.innerText = time;
+    let showAlarm = document.getElementById('alarm');
+    let showGreeting = document.getElementById('greet');
 
-    let message = 'Get up and stretch your legs'
-    let showAlarm = document.getElementById('alarm')
+    showAlarm.innerText = date.getFullYear();  //display full year eg: 2022 in corresponding element
 
+    //promp messages
+    let restMessage = 'Get up and stretch your legs'
+    let waterBreak = 'Get some water'
+
+    //greeting variables
     const greetingMorning = 'Good Morning'
     const greetingAfternoon = 'Good Afternoon'
     const greetingEvening = 'Good Evening'
-
-   
-    let showGreeting = document.getElementById('greet')
-        
-
-    if (minutes === 59) {
-    //   showAlarm.innerText = message;  
-      showTime.innerText = message
-    }else{
-      showAlarm.innerText = date.getFullYear();  
+      
+    //set conditionals to display variables based on personal desires
+    if (minutes === 59) { 
+      showTime.innerText = restMessage
     }
-
+    else if(minutes % 20 === 0){
+      showTime.innerText = waterBreak
+    }else{
+      showTime.innerText = time;
+    }
+     
+    //set conditionals to display greeting messages based on personal desires
     if (hours >= 24 && hours < 12){
-        showGreeting.innerText = greetingMorning
+      showGreeting.innerText = greetingMorning
     }
     else if (hours > 11 && hours < 16){
-        showGreeting.innerText = greetingAfternoon
+      showGreeting.innerText = greetingAfternoon
     }else{
-        showGreeting.innerText = greetingEvening
-    }
-       
-}
+      showGreeting.innerText = greetingEvening
+    }   
+  }
 setInterval(setTime, 1000) 
-
-
 }
 
 
